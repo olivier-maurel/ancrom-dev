ea7f3aacb3b7b824287e6a6402b31a30b2ff896b
olivier-maurel <maunav@protonmail.com>
2022-09-17 16:08:02 +0200

    float page + event key + map event key

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,6 +15,21 @@ class IndexController extends AbstractController
     {
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
+        ]);
+    }
+
+    #[Route('/change/page', name: 'app_change_page')]
+    public function contact(Request $request): Response
+    {
+        $page = $request->query->get('page');
+
+        if (is_file('../templates/index/_'.$page.'.html.twig'))
+            $html = $this->renderView('index/_'.$page.'.html.twig');
+        else 
+            $html = false;
+
+        return new JsonResponse([
+            'html' => $html
         ]);
     }
 }
